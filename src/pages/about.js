ac87ed34999435456805fb0f4a7fef44f736982b
Francis Coteur <fcoteur@hotmail.com>
2019-05-18 20:44:21 +0200

    small update

--- src/pages/about.js
+++ src/pages/about.js
@@ -10,7 +10,6 @@ export default function AboutPage() {
       <Head title='About' />
       <h1>About Page</h1>
       <p>these are my contact details</p>
-      <p>need a dev? <Link to='/contact'>contact me</Link></p>
       </Layout>
   )
 }
